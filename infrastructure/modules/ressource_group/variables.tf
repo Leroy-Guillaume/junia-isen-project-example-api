918381824a14243d28d89318769da3a059ea8524
Leroy-Guillaume <70961560+Leroy-Guillaume@users.noreply.github.com>
2024-12-15 16:30:15 +0100

    fix: ajout du fichier variables.tf manquant dans le module resource_group
    Ajout du fichier variables.tf manquant dans le module resource_group, qui avait été omis lors du précédent commit.
    
    Ce fichier inclut :
    - Les définitions des variables nécessaires pour le module resource_group (nom du groupe de ressources et région Azure).
    
    Cela corrige les erreurs rencontrées lors de l'initialisation Terraform.

---
+++ infrastructure/modules/ressource_group/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "Nom du groupe de ressources Azure"
+  type        = string
+}
+
+variable "location" {
+  description = "Région Azure pour le groupe de ressources"
+  type        = string
+}
